634387085c7e2796d4c187773b1ce566276a1767
Abhiram <abhiramsrikrishna@gmail.com>
2025-10-28 00:44:21 +0530

    minor changes

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,6 +1,22 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token found' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+    req.user = user;
+    next();
+  });
+};
+
 const protect = async (req, res, next) => {
   let token;
 
